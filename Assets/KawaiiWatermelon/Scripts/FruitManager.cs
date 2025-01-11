2124859e3777cc7ea97abad035c05b417c90ce0d
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2025-01-11 13:09:05 +0300

    adjust spawn line

--- Assets/KawaiiWatermelon/Scripts/FruitManager.cs
+++ Assets/KawaiiWatermelon/Scripts/FruitManager.cs
@@ -7,11 +7,16 @@ public class FruitManager : MonoBehaviour
     [Header("Elements")]
     [SerializeField] private GameObject fruitPrefab;
 
+    [Header("Settings")]
+    [SerializeField] private float fruitsYSpawnPosition;
+
+    [Header("Debug")]
+    [SerializeField] private bool enableGizmos;
+
     void Start()
     {
 
     }
-
 
     void Update()
     {
@@ -21,11 +26,25 @@ public class FruitManager : MonoBehaviour
 
     private void ManagePlayerInput()
     {
-        Instantiate(fruitPrefab, GetClickedWorldPosition(), Quaternion.identity);
+        Vector2 spawnPosition = GetClickedWorldPosition();
+        spawnPosition.y = fruitsYSpawnPosition;
+
+        Instantiate(fruitPrefab, spawnPosition, Quaternion.identity);
     }
 
     private Vector2 GetClickedWorldPosition()
     {
         return Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
+#if UNITY_EDITOR
+    // Show the line where the fruits will spawn
+    private void OnDrawGizmos()
+    {
+        if (!enableGizmos)
+            return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(-50, fruitsYSpawnPosition, 0), new Vector3(50, fruitsYSpawnPosition, 0));
+    }
+#endif
 }
